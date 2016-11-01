807c1fc2aaa3938c0a8fdec9ea33b1748d10d795
Allen Vartanian <avartanii@Allens-MacBook-Pro.local>
2016-10-31 22:00:06 -0700

    hello

--- _site/imgur2.js
+++ _site/imgur2.js
@@ -42,7 +42,7 @@ window.GiphySearchController = (() => {
                                     // }).text(image.title)
 
                                     $("<img/>").attr({
-                                        if (".is_album") {
+                                        if (.data(is_album)) {
                                             src: image.link
                                         } else {
                                            src: "https://i.vimeocdn.com/portrait/58832_300x300"
