c866d70c09bf83cd3cd5101d4c8b442d31697b76
Allen Vartanian <avartanii@Allens-MacBook-Pro.local>
2016-10-31 21:57:04 -0700

    hello

--- imgur2.js
+++ imgur2.js
@@ -36,19 +36,18 @@ window.GiphySearchController = (() => {
                                 //     src: image.link,
                                 //     alt: image.title
                                 // })
-                                    $("<a></a>").attr({
-                                        href: image.link
+                                    // $("<a></a>").attr({
+                                    //     href: image.link
                                         
-                                    }).text(image.title)
+                                    // }).text(image.title)
 
-                                    // $("<img/>").attr({
-                                    //     //if () {
-
-                                    //         src: image.link
-                                    //     //} else {
-                                    //       //  src: "https://i.vimeocdn.com/portrait/58832_300x300"
-                                    //     //}
-                                    // })
+                                    $("<img/>").attr({
+                                        if (".is_album") {
+                                            src: image.link
+                                        } else {
+                                           src: "https://i.vimeocdn.com/portrait/58832_300x300"
+                                        }
+                                    })
                                 );
                         })
                         );
